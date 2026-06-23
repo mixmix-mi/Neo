a563cb040e51a178582a8750073fb099640ca0ab
Moahmed Elsafy <amlahmel83@gmail.com>
2026-06-24 01:49:21 +0300

    Update M.lua

--- M.lua
+++ M.lua
@@ -1,1 +1,7 @@
+local Tabs = {
+    Misc = Window:Tab({ Title = "Misc", Icon = "solar:box-minimalistic-outline", Locked = false })
+}
 
+if not Tabs.Misc then
+    Tabs.Misc = Window:Tab({ Title = "Misc", Icon = "shapes", Locked = false })
+end
